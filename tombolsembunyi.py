85d6d12661d20bd614b5fa1620f30d65fe933c3a
Yt-Real <70317211+Yt-Real@users.noreply.github.com>
2020-08-28 05:25:54 +0700

    Add files via upload

---
+++ tombolsembunyi.py
@@ -0,0 +1,48 @@
+impor  os
+dari  waktu  tidur impor 
+
+
+a  = ' \ 033 [92m'
+b  = ' \ 033 [91m'
+c  = ' \ 033 [0m'
+
+ penyiapan def ():
+    coba :
+        os . mkdir ( '/data/data/com.termux/files/home/.termux' )
+    kecuali :
+        lulus
+    key  =  "extra-keys = [['ESC', '/', '-', 'HOME', 'UP', 'END', 'PGUP'], ['TAB', 'CTRL', 'ALT' , 'LEFT', 'DOWN', 'RIGHT', 'PGDN']] "
+    buka ( '/data/data/com.termux/files/home/.termux/termux.properties' , 'w' ). tulis ( kunci )
+    os . sistem ( 'termux-reload-settings' )
+
+
+def  spanduk ():
+    os . sistem ( 'jelas' )
+| _     _  _    ___                  _      
+( )   ( )( )_ |  _`\               (_ )    
+`\`\_/'/'| ,_)| (_) )   __     _ _  | |    
+  `\ /'  | |  | ,  /  /'__`\ /'_` ) | |    
+   | |   | |_ | |\ \ (  ___/( (_| | | |    
+   (_)   `\__)(_) (_)`\____)`\__,_)(___)   
+                                           
+                                           |
+
+cetak ( '' ' \ 033 [95m
++ -------------------------------------- +
+| jangan lupa like and subscribe nya |
++ -------------------------------------- +
+| Terimakasih | Yt-Real videos |
++ -------------------------------------- + '' ' )
+
+
+jika  __name__ == '__main__' :
+    spanduk ()
+    dari  threading,  import  Thread  sebagai  td
+    t  =  td ( target = setup )
+    t . mulai ()
+    sementara  t . is_alive ():
+        untuk  saya  di  "- \ | / - \ | /" :
+            print ( ' \ r Please wait' + i + '' , end = "" , flush = True )
+            tidur ( 0.1 )
+    spanduk ()
+    print ( c + 'jangan lupa like and subscribe nya ya' + a + 'selamat mencoba' + c + 'jaga kesehatan dan stay safe  ◜‿◝ ♡ ' )
